5914abb14159e5df52c87819e9630d7fe1cf89e1
Takshak Mudgal <takshakmudgal@gmail.com>
2023-02-05 12:09:00 +0530

    added prettier formatting

--- events.js
+++ events.js
@@ -1,22 +1,21 @@
-window.onscroll = function() {
-    const nav = document.querySelector('.nav-container');
-    if (window.pageYOffset > nav.offsetTop) {
-        nav.classList.add("sticky");
-        document.querySelector('.nav-container').style.position = "fixed";
-        document.querySelector('.nav-container').style.top = "0";
-        document.querySelector('.nav-container').style.width = "100%";
-        document.querySelector('.nav-container').style.zIndex = "100";
-    } else {
-        nav.classList.remove("sticky");
-        document.querySelector('.nav-container').style.position = "relative";
-    }
-}
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburgerToggle = document.getElementById("hamburger-toggle");
-    const hamburgerMenu = document.getElementById("hamburger-menu");
-    
-    hamburgerToggle.addEventListener("click", function() {
-      hamburgerMenu.classList.toggle("hidden");
-    });
+window.onscroll = function () {
+  const nav = document.querySelector(".nav-container");
+  if (window.pageYOffset > nav.offsetTop) {
+    nav.classList.add("sticky");
+    document.querySelector(".nav-container").style.position = "fixed";
+    document.querySelector(".nav-container").style.top = "0";
+    document.querySelector(".nav-container").style.width = "100%";
+    document.querySelector(".nav-container").style.zIndex = "100";
+  } else {
+    nav.classList.remove("sticky");
+    document.querySelector(".nav-container").style.position = "relative";
+  }
+};
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerToggle = document.getElementById("hamburger-toggle");
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+
+  hamburgerToggle.addEventListener("click", function () {
+    hamburgerMenu.classList.toggle("hidden");
   });
-  
+});
